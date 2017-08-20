afaa23d73de46f9d586ac698322599c5c0a93c23
Henrique Soares Barbosa <31192881+softlivre@users.noreply.github.com>
2017-08-20 19:54:34 -0300

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>phpinfo github first experience</title>
+</head>
+
+<body>
+
+<?php
+
+// || This is my first experience under github and has no meaning interest || It will simply call the phpinfo() function.
+echo "|| This is my first experience under github and has no meaning interest || It will simply call the phpinfo() function.<br /><br /><br />";
+phpinfo();
+
+	
+?>
+	
+
+
+
+</body>
+</html>
